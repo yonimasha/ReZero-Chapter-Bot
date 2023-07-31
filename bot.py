9ec23e4edcb036b39ed7cdd072ebcafdc7cc27a4
yonimasha <yonima2003@gmail.com>
2023-07-31 17:07:23 -0400

    finished implementing scrape.py file (forgot to push periodically) and also finished bot.py, testing still has to be done

--- bot.py
+++ bot.py
@@ -1,9 +1,26 @@
 import discord
-import requests
-import asyncio
-from bs4 import BeautifulSoup
+import scrape
+import time
+import random
+from datetime import datetime
+
+
+def create_message(chapters):
+
+    current_datetime = datetime.now()
+    current_clock_time = current_datetime.strftime("%I:%M %p")
+
+    message = f"Detected {len(chapters)} new chapter(s): \n\n"
+
+    for chapter, word_count in chapters.items():
+        message += f'• {chapter}\n\t-- There is roughly around {word_count} words in this chapter.\n\n'
+
+    message += "I will let you know if any more chapters appear in the future."
+
+    return message
 
 def run_discord_bot():
+
     TOKEN = "MTEzNDU3ODU3NTQwMDUwMTMxMA.GUK3Yx.9gZVgSA5c0RilQMnUUqv3vWS1JS_apd5cdIlTo"
     intents = discord.Intents.default()
     intents.message_content = True
@@ -13,5 +30,22 @@ def run_discord_bot():
     async def on_ready():
         print(f"{client.user} is now running.")
 
+        channel = client.get_channel(1134582687085109454)
+
+        while True:
+
+            new_chapters = scrape.get_newest_chapters(True)
+
+            if channel:
+                if len(new_chapters) > 0:
+                    message = create_message(new_chapters)
+                    await channel.send(message)
+
+                else:
+                    await channel.send('No new chapters as of now...')
+            else:
+                print(f'Channel not found.')
+
+            time.sleep(random.randint(5, 20))
 
     client.run(TOKEN)
---
+++ scrape.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import string
+
+
+def find_word_count(chap_link):
+    chapter_data = requests.get(chap_link)
+
+    chapter_content = BeautifulSoup(chapter_data.content, 'html.parser')
+
+    div = chapter_content.find('div', class_='entry-content')
+
+    paragraphs = div.find_all('p')
+
+    total_words = 0
+    for paragraph in paragraphs:
+        # remove punctuation from getting counted
+        translator = str.maketrans('', '', string.punctuation)
+        text_without_punctuation = paragraph.get_text().translate(translator)
+
+        # turn to a list to find the amount of words
+        words = text_without_punctuation.split()
+        words_in_paragraph = len(words)
+
+        total_words += words_in_paragraph
+
+    return total_words
+
+
+def update_chapter_count(file_name, chapters_size):
+    try:
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+            chapter_count = data.get('chapter_count', 0)
+            if chapters_size > chapter_count:
+                chapter_count += (chapters_size - chapter_count)  # increment the chapter count
+                return chapter_count  # return number of new chapters
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Error updating chapter count")
+
+    with open(file_name, 'w') as file:
+        json.dump({'chapter_count': chapter_count}, file, indent=4)
+
+    return 0  # if no new chapters return 0
+
+
+def get_newest_chapters(end_of_arc):
+
+    file_name = 'chapter_count.json'
+
+    # check to see if I input an !end message to signify the end of the current part
+    if end_of_arc:
+        url = 'https://witchculttranslation.com/arc-8/'
+    else:
+        # TODO: finish changing to next arc
+        url = 'https://witchculttranslation.com/arc-8/'
+
+    # scrape website data, specifically the li elements (chapters)
+    data = requests.get(url)
+
+    if data.status_code == 200:
+        arc_content = BeautifulSoup(data.content, 'html.parser')
+        div = arc_content.find('div', class_='entry-content')
+        items = div.find_all('li')
+
+        # not all li elements are chapters, so we have to filter
+        chapters = []
+        for item in items:
+            if item.text.startswith("Chapter"):
+                chapters.append(item)
+
+    new_chapter_count = update_chapter_count(file_name, len(chapters))
+
+    new_chapters_wc = {}  # dictionary containing chapters as keys and word count as values
+
+    if new_chapter_count > 0:
+        for ch in chapters[-new_chapter_count:]:
+            ch_link = ch.find('a')['href']
+            new_chapters_wc[ch.get_text()] = find_word_count(ch_link)
+
+    return new_chapters_wc
+
+
+get_newest_chapters(True)
